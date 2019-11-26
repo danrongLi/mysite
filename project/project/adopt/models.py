a17c71c1ced94ba96eea22d768a41515194133ae
Danrong Li <dl3333@columbia.edu>
2019-11-26 22:06:52 +0000

    EST and *

--- project/project/adopt/models.py
+++ project/project/adopt/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
+from django.utils.translation import gettext as _
+
+
+class Pet(models.Model):
+
+    name = models.CharField(
+            help_text=_('Name of Animal'),
+            max_length=50,
+    )
+
+
+    species = models.CharField(
+            help_text=_('Species of Animal'),
+            max_length=100,
+    )
+
+    birth_date = models.DateField(
+            help_text=_('Birth Date'),
+    )
+
+    MALE = 'Male'
+    FEMALE = 'Female'
+    OTHER = 'Other'
+
+    SEX_CHOICES=(
+            (MALE, 'Male'),
+            (FEMALE, 'Female'),
+            (OTHER, 'Other'),
+           
+    )
+
+    sex = models.CharField(
+            help_text=_('Sex of the pet'),
+            max_length=35,
+            choices=SEX_CHOICES,
+            default=OTHER,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+
+
 # Create your models here.
--- project/project/adopt/urls.py
+++ project/project/adopt/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-        path('',views.index),
+        path('list/',views.all_pets),
+        path('<int:pet_id>', views.pet_details),
 ]
