f20748eba9e60d9339bad54826578a57b598a3fe
Danrong Li <dl3333@columbia.edu>
2019-12-05 01:34:02 +0000

    paul lecture

---
+++ project/project/adopt/management/commands/import.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+import csv
+from adopt.models import Pet
+class Command(BaseCommand):
+    def add_arguments(self,parser):
+        parser.add_argument('csv_file')
+
+    def handle(self,*args,**options):
+        with open(options['csv_file']) as fp:
+            reader = csv.DictReader(fp)
+            data = list(reader)
+        
+        for item in data:
+            p=Pet(
+                    name = item['name'],
+                    species = item['species'],
+                    birth_date = item['birth_date'],
+                    sex = item['sex'],
+                    )
+            p.save()
+
+
--- project/project/adopt/views.py
+++ project/project/adopt/views.py
@@ -3,6 +3,7 @@ from .models import Pet
 from django.shortcuts import render
 # Create your views here.
 import random
+from .forms import PetForm
 def index(request):
     i=random.random()
     return HttpResponse('Hi! How are you (i) !!!')
@@ -19,3 +20,33 @@ def all_pets(request):
 def pet_details(request, pet_id):
     pet = Pet.objects.get(id=pet_id)
     return HttpResponse(pet.name)
+
+
+def add_pet(request):
+    if request.method == 'POST':
+        form = PetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(f'/adopt/list')
+    else:
+        form = PetForm()
+    context = {
+            'form': form,
+            }
+    return render(request,'adopt/edit.html',context)
+
+
+def edit_pet(request,pet_id):
+    pet = Pet.objects.get(id=pet_id)
+    if request.method == 'POST':
+        form = PetForm(request.POST, instance=pet)
+        if form.is_valid():
+            form.save()
+            return redirect(f'/adopt/{pet_id}')
+        # check data with form
+    else:
+        form = PetForm(instance=pet)
+        #build empty form
+    context = {'form':form,
+            }
+    return render(request,'adopt/edit.html',context)
